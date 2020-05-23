9d61ba45b4dbacabd6e0b55f32fe539826568260
Tony Thomas <tony.thomas@keyflow.se>
2020-05-23 18:09:33 +0200

    Post registration data to backend

--- src/app/services/participant/participant.service.ts
+++ src/app/services/participant/participant.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Participant } from '../../models/participant';
 import { AuthService } from '../auth/auth.service';
+import { environment } from '../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ParticipantService {
-  constructor(private http: HttpClient, private authenticationService: AuthService) {}
+  constructor(private http: HttpClient) {}
 
-  registerParticipant(participant: Participant) {
+  registerParticipant(participantData: Participant) {
     // You do it without auth
+    this.http
+      .post(`${environment.apiUrl}/v1/auth/register/`, participantData)
+      .pipe(
+        map((participant: Participant) => {
+          console.log(participant);
+        })
+      )
+      .subscribe();
   }
 }
